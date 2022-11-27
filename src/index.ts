58143ca97423678bc60f42a4cc2ceb7ee593d606
Benoit Welsch <benoitwelsch@protonmail.com>
2022-11-27 12:18:52 +0100

    pi

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
 import web from './web';
+import {
+  LedMatrix,
+  GpioMapping,
+  LedMatrixUtils,
+  PixelMapperType,
+} from 'rpi-led-matrix';
+import {Time} from './window/time';
 
-web();
+//web();
+
+const matrix = new LedMatrix(
+  {
+    ...LedMatrix.defaultMatrixOptions(),
+    rows: 32,
+    cols: 64,
+    hardwareMapping: GpioMapping.AdafruitHat,
+    pixelMapperConfig: LedMatrixUtils.encodeMappers({
+      type: PixelMapperType.U,
+    }),
+  },
+  {
+    ...LedMatrix.defaultRuntimeOptions(),
+    gpioSlowdown: 2,
+  }
+);
+
+const pixels = new Time();
+
+pixels.run();
+
+matrix.afterSync((mat, dt, t) => {
+  pixels.screen.forEach((p, x, y) => {
+    matrix.fgColor({r: p.r, g: p.g, b: p.b}).setPixel(x, y);
+  });
+
+  setTimeout(() => matrix.sync(), 0);
+});
+
+// Get it started
+matrix.sync();
