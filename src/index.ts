8dffaa2e2d82f6139757b1236689d741231f0ba8
Benoit-Welsch <benoitwelsch@protonamil.com>
2022-12-09 20:55:34 +0100

    Feat : Dev = web server only

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,32 @@
-import web from './web';
-import {
-  LedMatrix,
-  GpioMapping,
-  LedMatrixUtils,
-  PixelMapperType,
-} from 'rpi-led-matrix';
-import {Time} from './window/time';
+import {LedMatrix, GpioMapping} from 'rpi-led-matrix';
+import {Time} from './time';
 
-web();
+const matrix = new LedMatrix(
+  {
+    ...LedMatrix.defaultMatrixOptions(),
+    rows: 32,
+    cols: 64,
+    hardwareMapping: GpioMapping.AdafruitHat,
+    brightness: 75,
+    limitRefreshRateHz: 75,
+  },
+  {
+    ...LedMatrix.defaultRuntimeOptions(),
+    gpioSlowdown: 2,
+  }
+);
 
-// const matrix = new LedMatrix(
-//   {
-//     ...LedMatrix.defaultMatrixOptions(),
-//     rows: 32,
-//     cols: 64,
-//     hardwareMapping: GpioMapping.AdafruitHat,
-//     brightness: 75,
-//     limitRefreshRateHz: 75,
-//   },
-//   {
-//     ...LedMatrix.defaultRuntimeOptions(),
-//     gpioSlowdown: 2,
-//   }
-// );
+const time = new Time();
 
-// const pixels = new Time();
+matrix.afterSync(() => {
+  time.forEach((row, y) => {
+    row.forEach((p, x) => {
+      matrix.fgColor({r: p.r, g: p.g, b: p.b}).setPixel(x, y);
+    });
+  });
 
-// pixels.run();
+  setTimeout(() => matrix.sync(), 0);
+});
 
-// matrix.afterSync((mat, dt, t) => {
-//   pixels.screen.forEachFlat((p, y, x) => {
-//     matrix.fgColor({r: p.r, g: p.g, b: p.b}).setPixel(x, y);
-//   });
-
-//   setTimeout(() => matrix.sync(), 0);
-// });
-
-// // Get it started
-// matrix.sync();
+// Get it started
+matrix.sync();
